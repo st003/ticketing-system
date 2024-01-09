318ff7a7fdde90287c2cc19beff788518e983685
st003 <stuart.j.calcote@gmail.com>
2024-01-08 20:42:50 -0700

    Added TicketController
    Revised IndexController to use @RequestMapping

--- src/main/java/com/st003/ticketing/controllers/TicketsController.java
+++ src/main/java/com/st003/ticketing/controllers/TicketsController.java
@@ -4,10 +4,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class IndexController {
+public class TicketsController {
 
-    @RequestMapping("/")
+    @RequestMapping("/tickets")
     public String getIndex() {
-        return "index";
+        return "tickets";
     }
 }
---
+++ src/test/java/com/st003/ticketing/controllers/TicketsControllerTest.java
@@ -0,0 +1,23 @@
+package com.st003.ticketing.controllers;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+@WebMvcTest(TicketsController.class)
+public class TicketsControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    void getIndex() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("/tickets"))
+            .andExpect(status().isOk());
+    }
+}
