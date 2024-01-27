c6427827b760c0d619c905938541d80bc7e81ab5
st003 <stuart.j.calcote@gmail.com>
2024-01-26 18:42:35 -0700

    Updated some controller tests to check for roles

--- src/test/java/st003/ticketing/controllers/TicketsControllerTest.java
+++ src/test/java/st003/ticketing/controllers/TicketsControllerTest.java
@@ -5,19 +5,21 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import org.junit.jupiter.api.Test;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-@WebMvcTest(TicketsController.class)
+@SpringBootTest
+@AutoConfigureMockMvc
 public class TicketsControllerTest {
 
     @Autowired
     private MockMvc mvc;
 
     @Test
-    @WithMockUser
+    @WithMockUser(roles = {"CUSTOMER"})
     void getTickets() throws Exception {
         mvc.perform(MockMvcRequestBuilders.get("/tickets"))
             .andExpect(status().isOk());
