27660ecf1070d4874be5ea3c1f008176d5d0de36
st003 <stuart.j.calcote@gmail.com>
2024-02-12 19:16:15 -0700

    Added HTTP POST test for RegisterControllerTest

--- src/test/java/st003/ticketing/controllers/RegisterControllerTest.java
+++ src/test/java/st003/ticketing/controllers/RegisterControllerTest.java
@@ -1,5 +1,6 @@
 package st003.ticketing.controllers;
 
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.jupiter.api.Test;
@@ -10,6 +11,8 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import st003.ticketing.data.entities.AppUser;
 
 
 @SpringBootTest
@@ -23,5 +26,14 @@ public class RegisterControllerTest {
     void getRegister() throws Exception {
         mvc.perform(MockMvcRequestBuilders.get("/register"))
             .andExpect(status().isOk());
+    }
+
+    @Test
+    void postRegister() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.post("/register")
+                    .flashAttr("appUser", new AppUser("test"))
+                    .param("password", "password")
+                    .with(csrf())
+                ).andExpect(status().isOk());
     }
 }
