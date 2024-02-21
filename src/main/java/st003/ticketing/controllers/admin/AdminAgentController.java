5537d46c5f10fbd5801821136f0cb79329c1995e
st003 <stuart.j.calcote@gmail.com>
2024-02-20 19:44:47 -0700

    Added ability to modify existing agents

--- src/main/java/st003/ticketing/controllers/admin/AdminAgentController.java
+++ src/main/java/st003/ticketing/controllers/admin/AdminAgentController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -21,27 +22,36 @@ public class AdminAgentController {
         this.repo = repo;
     }
 
-    @GetMapping("/admin/agent")
-    public String getAdminAgent(Model model) {
-        model.addAttribute("appUser", new AppUser());
+    @GetMapping(value = {"/admin/agent", "/admin/agent/{id}"})
+    public String getAdminAgent(@PathVariable(required = false) Optional<String> id, Model model) {
+
+        AppUser u = new AppUser();
+        if (id.isPresent()) {
+            Optional<AppUser> foundAppUser = repo.findById(Long.parseLong(id.get()));
+            if (foundAppUser.isPresent()) u = foundAppUser.get();
+        }
+
+        model.addAttribute("appUser", u);
         return "admin/agent";
     }
 
     @PostMapping("/admin/agent")
-    public String postAdminAgent(@ModelAttribute AppUser appUser, @RequestParam String password, Model model) {
+    public String postAdminAgent(@ModelAttribute AppUser appUser, @RequestParam(required = false) Optional<String> password, Model model) {
 
         // check if email is taken
         Optional<AppUser> emailTaken = repo.findByEmail(appUser.getEmail());
         if (emailTaken.isPresent()) {
-            model.addAttribute("error", "An account with this email already exists");
-
-        } else {
-            appUser.setPassword(password);
-            repo.save(appUser);
-
-            model.addAttribute("success", "New agent added successfully");
+            // when trying to create or update with an in-use email
+            if (appUser.getId() == null || appUser.getId() != emailTaken.get().getId()) {
+                model.addAttribute("error", "An account with this email already exists");
+                return "admin/agent";
+            }
         }
 
+        if (password.isPresent()) appUser.setPassword(password.get());
+        repo.save(appUser);
+
+        model.addAttribute("success", "Your changes have been saved");
         return "admin/agent";
     }
 }
